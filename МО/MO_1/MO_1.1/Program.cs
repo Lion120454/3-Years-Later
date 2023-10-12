89ee0f72ac822c64847c791a147a34db2622e710
Lion120454 <117920260+Lion120454@users.noreply.github.com>
2023-10-12 21:21:29 +0700

    Add files via upload

---
+++ МО/MO_1/MO_1.1/Program.cs
@@ -0,0 +1,53 @@
+﻿//x^4-10x^3+36x^2+5x
+//3,00004051727295
+using System;
+
+namespace MO_1._1
+{
+    class Program
+    {
+        static double F(double arg)
+        {
+            return (Math.Pow(arg, 4)) - (10 * Math.Pow(arg, 3)) + (36 * Math.Pow(arg, 2)) + 5 * arg;
+        }
+        static void Main(string[] args)
+        {
+            double a = 3, b = 5, e = 0.0001;
+            //Метод Дихотомии
+            double l= 0.2 * e, y,z;
+            double x=0;
+            for (int i = 0; Math.Abs(b-a)>e; i++)
+            {
+                y = (a + b - l) / 2;
+                z = (a + b + l) / 2;
+                if (F(y) <= F(z))
+                {
+                    b = z;
+                }
+                if(F(y) > F(z))
+                {
+                    a = y;
+                }
+                x = (a + b) / 2;
+            }
+            Console.WriteLine("Метод Дихотомии:");
+            Console.WriteLine("x= {0}", x);
+            Console.WriteLine("f(x)= {0}",F(x));
+            //Метод Равномерного поиска
+            double x1=0,n = 100,min = 300;
+            for (int i = 1; i < n; i++)
+            {
+                x1 = a + i*( (b-a)/(n+1) );
+                if (F(x1) < min)
+                {
+                    x = x1;
+                }
+            }
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Метод Равномерного поиска:");
+            Console.WriteLine("x= {0}", x);
+            Console.WriteLine("f(x)= {0}", F(x));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ МО/MO_2/Program.cs
@@ -0,0 +1,61 @@
+﻿//x^4-10x^3+36x^2+5x
+using System;
+using System.Collections.Generic;
+using System.Linq;  
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MO_2
+{
+    class Program
+    {
+        static double F(double arg)
+        {
+            return (Math.Pow(arg, 4)) - (10 * Math.Pow(arg, 3)) + (36 * Math.Pow(arg, 2)) + 5 * arg;
+        }
+        static double F1(double arg)
+        {
+            return (4*Math.Pow(arg, 3)) - (30 * Math.Pow(arg, 2)) + (72 * arg) + 5;
+        }
+        static double F2(double arg)
+        {
+            return (12*Math.Pow(arg, 2)) - (60 * arg) + 72;
+        }
+        static void Main(string[] args)
+        {
+            double a=3,b=5, e = 0.0001;
+            //Метод Ньтона
+            double x0 = b,x1=x0;
+            while (Math.Abs(F1(x0)) > e)
+            {
+                x1 = x0 - (F1(x0) / F2(x0));
+                x0 = x1;
+            }
+            Console.Write("Метод Ньтона: ");
+            Console.WriteLine("x= {0}",x0);
+
+            //Метод средней точки
+            double z = 0;
+            while(Math.Abs(F(z))>e)
+            {
+                z = (a + b) / 2;
+                Console.WriteLine("[a,b]= {0},{1}",a,b);
+                Console.WriteLine("Z= {0}",z);
+                Console.WriteLine(F(z));
+                if (F(z) < 0)
+                {
+                    a = z;
+                }
+                else
+                {
+                    b = z;
+                }
+                x0 = z;
+                //Console.ReadKey();
+            }
+            Console.Write("Метод средней точки: ");
+            Console.WriteLine("x= {0}",x0);
+            Console.ReadKey();
+        }
+    }
+}
